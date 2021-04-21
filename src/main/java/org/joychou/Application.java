6c4789d34b9a6223b65f88c98e23a7d1e90d7008
shadowosock5 <shadowosock5@protonmail.com>
2021-04-21 11:22:26 +0800

    add swagger from com.spring4all

--- src/main/java/org/joychou/Application.java
+++ src/main/java/org/joychou/Application.java
@@ -1,5 +1,6 @@
 package org.joychou;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,7 +11,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @ServletComponentScan // do filter
 @SpringBootApplication
-// @EnableEurekaClient  // 测试Eureka请打开注释，防止控制台一直有warning
+@EnableEurekaClient   // 测试Eureka请打开注释，防止控制台一直有warning
+@EnableSwagger2Doc
 public class Application extends SpringBootServletInitializer {
 
     @Override
